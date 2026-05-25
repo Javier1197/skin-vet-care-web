f56a2e9501361a0ce8e9d42566cc29c071139b41
Javier1197 <javier.torres1197@alumno.udg.mx>
2026-05-24 22:03:43 -0600

    Preparando integración Supabase

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Skin Vet Care - Sistema iniciado correctamente.");
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+// Configura estas llaves con los valores de tu panel de Supabase
+const supabaseUrl = 'https://hlmmaypouknygztwzlke.supabase.co/rest/v1/';
+const supabaseKey = 'sb_publishable_bLAkyrgj9kTACrPRNCmoVQ_HgZAhW4Y';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function cargarMascotas() {
+    const { data, error } = await supabase.from('mascotas').select('*');
+    if (error) console.error("Error:", error);
+    else console.log("Mascotas registradas:", data);
+}
+
+// Inicialización
+console.log("Skin Vet Care - Sistema conectado a Supabase");
+cargarMascotas();
